ef61383900c7055ae07ba5961e4ac88f9f54b3e2
okamis <henningphan@hotmail.com>
2014-10-12 00:29:51 +1100

    added getProfile to utility, Removed a print from findNextStudent

--- My/Utility.pm
+++ My/Utility.pm
@@ -5,7 +5,7 @@ use List::Util qw(first min);
 
 use Exporter qw(import);
 
-our @EXPORT_OK = qw(findStudent findNextStudent);
+our @EXPORT_OK = qw(findStudent findNextStudent getProfile);
 
 sub findStudent($){  # Returns bool if found
   my($username) =@_;
@@ -28,12 +28,39 @@ sub findNextStudent($$){ # Returns username if found else empty string
   if( not defined $idx or $idx > $#students ){
     return @students[0..9];
   }
-  print "$idx $students[$idx]\n";
   my $idxEnd = min($idx+$count-1, $#students);
   my @temp = @students[$idx..$idxEnd];
   return \@temp;
 
 }
+sub getProfile($){ # Returns a hash<string, \@>
+  my($username) = @_;
+  open F,"<","./students/$username/profile.txt" or
+    die "cant open file ./students/$username/profile.txt\n";
 
+  my @profileArr;
+  while( my $line = <F> ){
+      chomp $line;
+      push @profileArr, $line;
+  }
+  close F;
+
+  my %profile;
+  my $idx = 0;
+  while ($idx < @profileArr){
+    my $key = $profileArr[$idx];
+    $key =~ s/:$//;
+    my @values;
+    $idx = $idx+1;
+    while($idx < @profileArr){
+      if( $profileArr[$idx] =~ /^\w/){ last;}
+      $profileArr[$idx] =~ s/^\s*//;
+      push @values, $profileArr[$idx];
+      $idx = $idx+1;
+    }
+    $profile{$key} = \@values;
+  }
+  return \%profile;
+}
 
 1;
