eef57c9a66ad48f2bf4d8700f389fb318b589f5e
okamis <henningphan@hotmail.com>
2014-10-12 14:06:48 +1100

    utility has getPreferences

--- My/Utility.pm
+++ My/Utility.pm
@@ -5,7 +5,7 @@ use List::Util qw(first min);
 
 use Exporter qw(import);
 
-our @EXPORT_OK = qw(findStudent findNextStudent getProfile getPartProfile attrMatch);
+our @EXPORT_OK = qw(findStudent findNextStudent getProfile getPartProfile attrMatch getPreference);
 
 sub findStudent($){  # Returns bool if found
   my($username) =@_;
@@ -83,6 +83,38 @@ sub attrMatch($$$){
     if( $v eq $value){ return 1; }
   }
   return 0;
+}
+sub getPreference($){
+  my($username) = @_;
+  open F, "<", "./students/$username/preferences.txt" or
+    die "can't open file ./students/$username/preferences.txt\n";
+  my %pref;
+  my @prefArr;
+  while( my $line = <F> ){
+      chomp $line;
+      push @prefArr, $line;
+  }
+  close F;
+
+  my %preference;
+  my $idx = 0;
+  while ($idx < @prefArr){
+    my $key = $prefArr[$idx];
+    $key =~ s/:$//;
+    my @values;
+    $idx = $idx+1;
+    while($idx < @prefArr){
+      if( $prefArr[$idx] =~ /^\w/){ last;}
+      $prefArr[$idx] =~ s/^\s*//;
+      push @values, $prefArr[$idx];
+      $idx = $idx+1;
+    }
+    $preference{$key} = \@values;
+  }
+  return \%preference;
+
+
+
 }
 
 1;
